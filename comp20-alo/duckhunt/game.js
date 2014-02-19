7f1ecda2fbb9e269d4ac4a8ba8fd07230b7097ba
Adrian Lo <adrian.lo@tufts.edu>
2014-02-19 16:49:07 -0500

    HTML now calls draw function in game.js

--- comp20-alo/duckhunt/game.js
+++ comp20-alo/duckhunt/game.js
@@ -1,1 +1,12 @@
 // Your work goes here...
+function draw() {
+	if (game.getContext) {
+        ctx = game.getContext('2d');
+		img = new Image();
+		img.onload=function() {
+			ctx.drawImage(img, 10, 10);
+			}
+		img.src = 'assets/duckhunt.png';
+		
+		}
+	}
