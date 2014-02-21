7219d00a6b6620dc35c19d428e75cfc9531b3041
Adrian Lo <adrian.lo@tufts.edu>
2014-02-21 16:28:09 -0500

    Dirt road and bushes are now drawn

--- comp20-alo/duckhunt/game.js
+++ comp20-alo/duckhunt/game.js
@@ -2,11 +2,34 @@
 function draw() {
 	if (game.getContext) {
         ctx = game.getContext('2d');
+/*old code
+		height=600;
+		width=800;
 		img = new Image();
 		img.onload=function() {
-			ctx.drawImage(img, 10, 10);
+			ctx.drawImage(img, 0, 0, width, height);
 			}
-		img.src = 'assets/duckhunt.png';
-		
+*/
+		//drawing the ground
+		drawGround();
+		function drawGround() {
+			sourceX=0;
+			sourceY=700;
+			sourceWidth=900;
+			sourceHeight=200;
+			destX=0;
+			destY=422;
+			destWidth=800;
+			destHeight=178;
+			drawObject(sourceX, sourceY, sourceWidth, sourceHeight, destX, destY, destWidth, destHeight);
+			}
+		//draws a portion of the sprite sheet onto the canvas
+		function drawObject(sourceX, sourceY, sourceWidth, sourceHeight, destX, destY, destWidth, destHeight) {
+			img = new Image();
+			img.onload=function() {
+				ctx.drawImage(img, sourceX, sourceY, sourceWidth, sourceHeight, destX, destY, destWidth, destHeight);
+				}
+			img.src = 'assets/duckhunt.png';
+			}
 		}
 	}
