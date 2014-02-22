648def0af2cc6abb91b0362e1d5f32947c764d3b
Adrian Lo <adrian.lo@tufts.edu>
2014-02-22 13:05:19 -0500

    Sky is now drawn, JS code cleaned up

--- comp20-alo/duckhunt/game.js
+++ comp20-alo/duckhunt/game.js
@@ -1,35 +1,34 @@
-// Your work goes here...
 function draw() {
 	if (game.getContext) {
         ctx = game.getContext('2d');
-/*old code
-		height=600;
-		width=800;
-		img = new Image();
-		img.onload=function() {
-			ctx.drawImage(img, 0, 0, width, height);
-			}
-*/
+
+		//drawing the sky
+		ctx.fillStyle="#87CEEB";
+		ctx.fillRect(0, 0, 800, 600);
+
 		//drawing the ground
 		drawGround();
-		function drawGround() {
-			sourceX=0;
-			sourceY=700;
-			sourceWidth=900;
-			sourceHeight=200;
-			destX=0;
-			destY=422;
-			destWidth=800;
-			destHeight=178;
-			drawObject(sourceX, sourceY, sourceWidth, sourceHeight, destX, destY, destWidth, destHeight);
-			}
-		//draws a portion of the sprite sheet onto the canvas
-		function drawObject(sourceX, sourceY, sourceWidth, sourceHeight, destX, destY, destWidth, destHeight) {
-			img = new Image();
-			img.onload=function() {
-				ctx.drawImage(img, sourceX, sourceY, sourceWidth, sourceHeight, destX, destY, destWidth, destHeight);
-				}
-			img.src = 'assets/duckhunt.png';
-			}
-		}
 	}
+}
+
+//defines arguments pertaining to the ground and draws by calling drawObject
+function drawGround() {
+	sourceX=0;
+	sourceY=700;
+	sourceWidth=900;
+	sourceHeight=200;
+	destX=0;
+	destY=422;
+	destWidth=800;
+	destHeight=178;
+	drawObject(sourceX, sourceY, sourceWidth, sourceHeight, destX, destY, destWidth, destHeight);
+}
+
+//draws a portion of the sprite sheet onto the canvas
+function drawObject(sourceX, sourceY, sourceWidth, sourceHeight, destX, destY, destWidth, destHeight) {
+	img = new Image();
+	img.onload=function() {
+		ctx.drawImage(img, sourceX, sourceY, sourceWidth, sourceHeight, destX, destY, destWidth, destHeight);
+	}
+	img.src = 'assets/duckhunt.png';
+}
