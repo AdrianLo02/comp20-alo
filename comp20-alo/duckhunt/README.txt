e52eedca9e4be3c5476d3a7a1b65711f08da3026
Adrian Lo <adrian.lo@tufts.edu>
2014-02-22 16:40:57 -0500

    Tree and dog are now drawn (with some problems)

--- comp20-alo/duckhunt/README.txt
+++ comp20-alo/duckhunt/README.txt
@@ -1,6 +1,6 @@
 Assignment 2: Duck Hunt, Part 1
 
-1. HTML calls the draw function in game.js. JS currently draws entire sprite sheet onto the canvas.
+1. HTML calls the draw function in game.js. Sky, grass, dirt, tree and dog are all drawn onto the canvas. Currently there is an issue where the tree, grass/dirt and dog will be drawn in a random order.
 
 2. I have not collaborated with anyone for this assignment.
 
--- comp20-alo/duckhunt/game.js
+++ comp20-alo/duckhunt/game.js
@@ -6,26 +6,52 @@ function draw() {
 		ctx.fillStyle="#87CEEB";
 		ctx.fillRect(0, 0, 800, 600);
 
+		drawTree();
 		//drawing the ground
 		drawGround();
+		drawDog();
 	}
+}
+
+function drawTree() {
+	sourceX=0;
+	sourceY=268;
+	sourceWidth=80;
+	sourceHeight=130;
+	destX=0;
+	destY=120;
+	destWidth=sourceWidth*3;
+	destHeight=sourceHeight*3;
+	drawSprite(sourceX, sourceY, sourceWidth, sourceHeight, destX, destY, destWidth, destHeight);
 }
 
 //defines arguments pertaining to the ground and draws by calling drawObject
 function drawGround() {
-	sourceX=0;
+	sourceX=100;
 	sourceY=700;
-	sourceWidth=900;
+	sourceWidth=800;
 	sourceHeight=200;
 	destX=0;
-	destY=422;
-	destWidth=800;
-	destHeight=178;
-	drawObject(sourceX, sourceY, sourceWidth, sourceHeight, destX, destY, destWidth, destHeight);
+	destY=400;
+	destWidth=sourceWidth
+	destHeight=sourceHeight
+	drawSprite(sourceX, sourceY, sourceWidth, sourceHeight, destX, destY, destWidth, destHeight);
+}
+
+function drawDog() {
+	sourceX=0;
+	sourceY=0;
+	sourceWidth=61;
+	sourceHeight=43;
+	destX=150;
+	destY=430;
+	destWidth=sourceWidth*3;
+	destHeight=sourceHeight*3;
+	drawSprite(sourceX, sourceY, sourceWidth, sourceHeight, destX, destY, destWidth, destHeight);
 }
 
 //draws a portion of the sprite sheet onto the canvas
-function drawObject(sourceX, sourceY, sourceWidth, sourceHeight, destX, destY, destWidth, destHeight) {
+function drawSprite(sourceX, sourceY, sourceWidth, sourceHeight, destX, destY, destWidth, destHeight) {
 	img = new Image();
 	img.onload=function() {
 		ctx.drawImage(img, sourceX, sourceY, sourceWidth, sourceHeight, destX, destY, destWidth, destHeight);
