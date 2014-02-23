afab0dce4f71a6df777d226238003965e7eb1029
Adrian Lo <adrian.lo@tufts.edu>
2014-02-23 16:35:04 -0500

    5 ducks are now drawn; assignment 2 complete

--- comp20-alo/duckhunt/game.js
+++ comp20-alo/duckhunt/game.js
@@ -9,13 +9,19 @@ function draw() {
 		ctx.fillStyle="#87CEEB";
 		ctx.fillRect(0, 0, 800, 600);
 
-		//drawing from the sprite sheet
+		//drawing from the sprite sheet. Each of the following functions defines values for which sprite to use and where to place it on the canvas, then calls another function to draw
+		drawDuck1();
+		drawDuck2();
+		drawDuck3();
+		drawDuck4();
+		drawDuck5();
 		drawTree();
 		drawGround();
 		drawDog();
 	}
 }
 
+//defines values signifying what portion of the sprite sheet to use and where to draw on the canvas, then draws by calling drawSprite
 function drawTree() {
 	sourceX=0;
 	sourceY=268;
@@ -28,7 +34,6 @@ function drawTree() {
 	drawSprite(sourceX, sourceY, sourceWidth, sourceHeight, destX, destY, destWidth, destHeight);
 }
 
-//defines values signifying what portion of the sprite sheet to use and where to draw on the canvas, then draws by calling drawSprite
 function drawGround() {
 	sourceX=100;
 	sourceY=700;
@@ -53,24 +58,64 @@ function drawDog() {
 	drawSprite(sourceX, sourceY, sourceWidth, sourceHeight, destX, destY, destWidth, destHeight);
 }
 
-function drawDuck1 {
-
+function drawDuck1() {
+	sourceX=0;
+	sourceY=120;
+	sourceWidth=38;
+	sourceHeight=34;
+	destX=50;
+	destY=50;
+	destWidth=sourceWidth*3;
+	destHeight=sourceHeight*3;
+	drawSprite(sourceX, sourceY, sourceWidth, sourceHeight, destX, destY, destWidth, destHeight);
 }
 
-function drawDuck2 {
-	
+function drawDuck2() {
+	sourceX=130;
+	sourceY=120;
+	sourceWidth=38;
+	sourceHeight=34;
+	destX=600;
+	destY=150;
+	destWidth=sourceWidth*3;
+	destHeight=sourceHeight*3;
+	drawSprite(sourceX, sourceY, sourceWidth, sourceHeight, destX, destY, destWidth, destHeight);
 }
 
-function drawDuck3 {
-	
+function drawDuck3() {
+	sourceX=213;
+	sourceY=156;
+	sourceWidth=38;
+	sourceHeight=34;
+	destX=300;
+	destY=200;
+	destWidth=sourceWidth*3;
+	destHeight=sourceHeight*3;
+	drawSprite(sourceX, sourceY, sourceWidth, sourceHeight, destX, destY, destWidth, destHeight);
 }
 
-function drawDuck4 {
-	
+function drawDuck4() {
+	sourceX=305;
+	sourceY=232;
+	sourceWidth=38;
+	sourceHeight=34;
+	destX=700;
+	destY=350;
+	destWidth=sourceWidth*3;
+	destHeight=sourceHeight*3;
+	drawSprite(sourceX, sourceY, sourceWidth, sourceHeight, destX, destY, destWidth, destHeight);
 }
 
-function drawDuck5 {
-	
+function drawDuck5() {
+	sourceX=0;
+	sourceY=238;
+	sourceWidth=38;
+	sourceHeight=34;
+	destX=450;
+	destY=100;
+	destWidth=sourceWidth*3;
+	destHeight=sourceHeight*3;
+	drawSprite(sourceX, sourceY, sourceWidth, sourceHeight, destX, destY, destWidth, destHeight);
 }
 
 //draws a portion of the sprite sheet onto the canvas by calling drawImage
